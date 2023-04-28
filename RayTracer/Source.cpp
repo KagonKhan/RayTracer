d34e4e00a5d00608936f79ecc8133b15bf8e65fd
KagonKhan <s.szynk98@gmail.com>
2023-04-28 19:55:06 +0200

    working compile time "ray tracing"

--- RayTracer/Source.cpp
+++ RayTracer/Source.cpp
@@ -1,27 +1,39 @@
 #include "Vector.h"
 #include "Canvas.hpp"
 #include <chrono>
+
+
+
+
 template<typename Canvas>
 constexpr void render(Canvas& c) {
     for (int row = 0; row < Canvas::height; row++) {
         for (int col = 0; col < Canvas::width; col++) {
-            c.canvas[row][col] = Color<double>(row, col, row+col).clamped();
+            c.canvas[row][col] = Color<double>(row % 256, col % 256, (row*col)%256).clamped();
         }
     }
 }
 using namespace std::literals::chrono_literals;
 
-int main() {
-    Canvas<7680, 4320, double> c;
-    
-    auto t1 = std::chrono::high_resolution_clock::now();
+constexpr auto process() {
+    Canvas<256, 256, double> c;
     render(c);
-    auto t2 = std::chrono::high_resolution_clock::now();
+    return c;
+}
 
+int main() {
+
+    //Canvas<7680, 4320, double> c;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    constexpr auto x = process();
+    auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1) << std::endl;
 
+
+
     auto t3 = std::chrono::high_resolution_clock::now();
-    c.toPPM();
+    x.toPPM();
     auto t4 = std::chrono::high_resolution_clock::now();
 
 
